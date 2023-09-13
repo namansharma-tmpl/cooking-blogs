c8fb2a51c03d81e1dfa1e953721341f9c883c62c
Naman <bondj7938@gmail.com>
2023-09-13 13:08:51 +0530

    search api updated

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ const blogsRoutes = require('../routes/blogs');
 const recipesRoutes = require('../routes/recipes');
 const contactRoutes = require('../routes/contact');
 const newsletterRoutes = require('../routes/newsletter');
+const searchRoutes = require('../routes/search');
 
 module.exports = function (app){
     app.use('/api/home', homeRoutes);
@@ -10,4 +11,5 @@ module.exports = function (app){
     app.use('/api/recipes', recipesRoutes);
     app.use('/api/contact-us', contactRoutes);
     app.use('/api/newsletter-subscribe', newsletterRoutes);
+    app.use('/api/search', searchRoutes);
 }
