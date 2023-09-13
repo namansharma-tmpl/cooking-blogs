38533ff2f609779bf8d37b39e35535496cc257dd
Naman <bondj7938@gmail.com>
2023-09-13 05:34:32 +0530

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 require('dotenv').config();
 const express = require('express');
 const db = require('./models/index');
+const cors = require('cors');
 
 const app = express();
 const configs = require('./config/routes');
@@ -19,6 +20,7 @@ async function auth_db_conn(){
 
 auth_db_conn();
 
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));  // for parsing application/x-www-form-urlencoded
 
